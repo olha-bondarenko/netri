365544e4b44abdd66e20fdeb29a590ec18088a4b
olha-bondarenko <olga.bondarenko.nl@gmail.com>
2022-08-28 17:29:31 +0200

    init commit

--- backend/src/configs/database.ts
+++ backend/src/configs/database.ts
@@ -1,7 +1,7 @@
 import {connect, ConnectOptions} from 'mongoose';
 
 export const dbConnect = () => {
-    connect('mongodb+srv://olgaB:uwKO7xLf6PFrMzUm@olga.rm2mx.azure.mongodb.net/?retryWrites=true&w=majority', {
+    connect(process.env.MONGO_URI!, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     } as ConnectOptions).then(
