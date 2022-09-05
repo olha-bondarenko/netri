f50fb26515ab3d6d0a06c4eb270220bad9d1bcba
olha-bondarenko <olga.bondarenko.nl@gmail.com>
2022-09-05 21:38:00 +0200

    wip: heroku deploy

--- src/server.ts
+++ src/server.ts
@@ -12,14 +12,12 @@ dbConnect();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cors({
-    origin: "http://localhost:3000"
-}));
+app.use(cors());
 
 app.use("/api/posts", postsRouter);
 app.use("/api/user", userRouter);
 
-app.use(express.static('public'));
+app.use(express.static('frontend/build'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, err => {
