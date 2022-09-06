2f8356d98627a31409c581b15031c21bec040b31
olha-bondarenko <olga.bondarenko.nl@gmail.com>
2022-09-06 13:12:48 +0200

    heroku

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,17 +8,16 @@ import PostDetails from './components/PostDetails/PostDetails';
 
 const App = () => {
   const user = JSON.parse(localStorage.getItem('profile'));
-  let hashHistory = Router.hashHistory;
   return (
-    <Router history={hashHistory}>
+    <Router>
        <Container maxWidth='xl'>
        <Navbar/>
        <Routes>
-         <Route path="/" exact element={<Navigate to="posts" />}/>
-         <Route path="/posts" exact element={<Home />}/>
-         <Route path="/posts/search" exact element={<Home />}/>
-         <Route path='/posts/:id' exact element={<PostDetails />}/>
-         <Route path='/signin' exact element={ !user ? <Auth /> : <Navigate to="/posts"/>}/>
+         <Route exact path="/" element={<Navigate to="posts" />}/>
+         <Route path="/posts" element={<Home />}/>
+         <Route path="/posts/search" element={<Home />}/>
+         <Route path='/posts/:id' element={<PostDetails />}/>
+         <Route exact path='/signin' element={ !user ? <Auth /> : <Navigate to="/posts"/>}/>
        </Routes>
       </Container>
     </Router>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,11 +7,11 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { reducers } from './reducers';
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(reducers, {}, compose(applyMiddleware(thunk)));
 
-const root = createRoot(document.getElementById('root'));
-root.render(  
-<Provider store={store}>
-  <App />
-</Provider>);
-
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
