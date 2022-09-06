9582f62e07c3bc902a3a9f51e4919c459da29263
olha-bondarenko <olga.bondarenko.nl@gmail.com>
2022-09-06 15:24:01 +0200

    heroku

--- src/server.ts
+++ src/server.ts
@@ -21,9 +21,8 @@ app.use("/api/user", userRouter);
 app.use(express.static('frontend/build'));
 
 app.get('*', (req: any, res: any) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'))
 })
-
 const port = process.env.PORT || 5000;
 app.listen(port, err => {
     if(err) throw err;
